5c0efdaf44ec1c1f70ca9da0c5ef70f01ebffbe6
Juan Manuel González Garzón <jm.gonzalez1844@uniandes.edu.co>
2025-09-20 10:00:55 -0500

    Agregar user repository.

---
+++ Api/src/authentication/repositories/user.repository.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { Knex } from 'knex';
+
+@Injectable()
+export class UserRepository {
+  constructor(@Inject('KNEX_CONNECTION') private readonly knex: Knex) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.knex<User>('users').select('*').orderBy('id', 'asc');
+  }
+
+  async findMany(limit: number): Promise<User[]> {
+    const l = Number(limit) > 0 ? Math.floor(Number(limit)) : 10;
+    return await this.knex<User>('users')
+      .select('*')
+      .orderBy('id', 'asc')
+      .limit(l);
+  }
+  // Aquí irían los métodos para interactuar con la base de datos
+
+  async findById(id: number): Promise<User[]> {
+    return await this.knex<User>('users')
+      .select('*')
+      .where({ id })
+      .orderBy('id', 'asc')
+      .limit(1);
+  }
+}
